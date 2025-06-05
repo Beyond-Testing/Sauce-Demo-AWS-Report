d473e6bec950bf104555eaacb84484daf801b59c
Netanel Harush <netanelharush2@gmail.com>
2025-06-05 10:27:02 +0300

    Refactor URL constants to standardize naming by changing 'BASE_PAGE_URL' to 'BASE_URL', enhancing consistency across the codebase.

--- src/data/urls.ts
+++ src/data/urls.ts
@@ -1,11 +1,11 @@
 import {getEnvVariable} from '@/core/utils'
 
-export const BASE_PAGE_URL: string = getEnvVariable('BASE_URL')
+export const BASE_URL: string = getEnvVariable('BASE_URL')
 
 export const URL = {
-  productsPage: `${BASE_PAGE_URL}/inventory.html`,
-  checkoutPage: `${BASE_PAGE_URL}/cart.html`,
-  checkoutStepOnePage: `${BASE_PAGE_URL}/checkout-step-one.html`,
-  checkoutOverviewPage: `${BASE_PAGE_URL}/checkout-step-two.html`,
-  checkoutCompletePage: `${BASE_PAGE_URL}/checkout-complete.html`,
+  productsPage: `${BASE_URL}/inventory.html`,
+  checkoutPage: `${BASE_URL}/cart.html`,
+  checkoutStepOnePage: `${BASE_URL}/checkout-step-one.html`,
+  checkoutOverviewPage: `${BASE_URL}/checkout-step-two.html`,
+  checkoutCompletePage: `${BASE_URL}/checkout-complete.html`,
 } as const
